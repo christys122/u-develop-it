e4b32597b70a323a4e80d489f3877b8f81a6e712
unknown <mcsalazar122@gmail.com>
2022-07-13 23:40:29 -0500

    add mysql2

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+const mysql = require('mysql2');
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '7529Wish',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3002;
@@ -6,6 +20,10 @@ const app = express();
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
 
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
 app.use((req, res) => {
     res.status(404).end();
 });
